f24f70975af871bddd1555849d3962b0d5852c16
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2024-03-05 23:50:51 +0530

    added the cors

--- server/server.js
+++ server/server.js
@@ -11,7 +11,13 @@ dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 
 app.use(cors());
 app.use(compression({ threshold: 2048 }));
